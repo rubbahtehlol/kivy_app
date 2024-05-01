1fcade91f2af82acd850c30258af61fc2642c9f3
rubbahtehlol <peterrokstad@gmail.com>
2024-05-01 14:18:45 +0200

    Moved many functions over to DatabaseOperations

--- app/backend/database/mongo_db.py
+++ app/backend/database/mongo_db.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient, errors
 from datetime import datetime, timedelta
+import re
+from collections import defaultdict
 
 class DatabaseOperations:
     def __init__(self, uri='mongodb://localhost:27017/', db_name='receiptsys'):
@@ -11,40 +13,157 @@ class DatabaseOperations:
             print("Failed to connect to MongoDB:", err)
             self.db = None
 
-    def login_user(self, username, password):
-        user = self.db['user_profiles'].find_one({"user_id": username})
-        return user if user and user.get("password") == password else None
-
-    def create_user_profile(self, user_id, price_sensitivity, password):
-        profile = {
-            "user_id": user_id,
-            "password": password,  # Remember to hash in a real app
-            "preferences": {
-                "price_sensitivity": price_sensitivity
-            },
-            "purchase_history": []
-        }
-        return self.db['user_profiles'].insert_one(profile).inserted_id
+    def insert_one(self, collection, data):
+        try:
+            self.db[collection].insert_one(data)
+            return True
+        except errors.PyMongoError as err:
+            print("Failed to insert data:", err)
+            return False
     
-    def find_lowest_prices_for_basket(self, basket_items):
-        receipts = self.db['receipts']
+    def insert_many(self, collection, data):
+        try:
+            self.db[collection].insert_many(data)
+            return True
+        except errors.PyMongoError as err:
+            print("Failed to insert data:", err)
+            return False
         
-        time_frames = [1, 7, 14, 30]
-        results = {}
-
+    def find_one(self, collection, query):
+        return self.db[collection].find_one(query)
+    
+    def find_many(self, collection, query):
+        return self.db[collection].find(query)
+    
+    def update_one(self, collection, query, data):
+        try:
+            self.db[collection].update_one(query, {"$set": data})
+            return True
+        except errors.PyMongoError as err:
+            print("Failed to update data:", err)
+            return False
+    
+    def find_lowest_prices_for_basket(self, basket_items, days_back=None):
+        results = []
         for item in basket_items:
-            results[item] = {}
-            for days in time_frames:
-                start_date = datetime.now() - timedelta(days=days)
-                pipeline = [
-                    {"$match": {"items.name": item, "purchase_date": {"$gte": start_date}}},
-                    {"$unwind": "$items"},
-                    {"$match": {"items.name": item}},
-                    {"$group": {"_id": "$items.name", "lowest_price": {"$min": "$items.price"}}}
-                ]
-                lowest_price = list(receipts.aggregate(pipeline))
-                if lowest_price:
-                    results[item][f'{days} days'] = lowest_price[0]['lowest_price']
-                else:
-                    results[item][f'{days} days'] = "No data"
+            if not item.strip():
+                continue
+            results.append(self.query_grocery_prices(item.upper(), days_back))
         return results
+    
+    def query_grocery_prices(self, item_name, days_back):
+        groceries = self.db['groceries']
+
+        start_date = (datetime.now() - timedelta(days=days_back)).strftime("%d.%m.%Y %H:%M") if days_back else None
+        query = {"name": item_name, "price_history.date": {"$gte": start_date}} if start_date else {"name": item_name}
+        pipeline = [
+            {'$match': query},
+            {'$unwind': '$price_history'},
+            {'$sort': {'price_history.store': 1, 'price_history.date': -1}},
+            {'$group': {
+                '_id': '$price_history.store',
+                'latestDate': {'$first': '$price_history.date'},
+                'minPrice': {'$min': '$price_history.price'},
+                'itemName': {'$first': '$name'}
+            }},
+            {'$group': {
+                '_id': '$itemName',
+                'stores': {'$push': {'store': '$_id', 'date': '$latestDate', 'price': '$minPrice'}}
+            }}
+        ]
+        result = list(groceries.aggregate(pipeline))
+        return result[0] if result else {'_id': item_name, 'stores': []}
+    
+    def fetch_store_distances(self, user_location, prices):
+        involved_stores = {store['store'].lower() for price_info in prices for store in price_info['stores']}
+
+        if not involved_stores:
+            return None
+
+        # Prepare an $or query with case-insensitive regex matches for each store
+        regex_query = [{'name': re.compile(f'^{re.escape(store)}$', re.IGNORECASE)} for store in involved_stores]
+        temp_data = self.db['stores'].find({'$or': regex_query})
+        store_data = {store['name'].lower(): store for store in temp_data}
+
+        # Calculate distances using names directly from store_data
+        distances = {}
+        for store_name, store_info in store_data.items():
+            if 'location' in store_info and 'coordinates' in store_info['location']:
+                store_location = store_info['location']['coordinates']
+                distances[store_name] = self.calculate_distance(user_location, store_location)
+            else:
+                print(f"Location data missing for store: {store_name}")
+                distances[store_name] = None  # or handle as needed
+
+        return distances
+    
+    def calculate_store_scores(self, prices, distances, price_sensitivity):
+        # Log the distances to verify their availability
+        print("Distances available:", distances)
+
+        # Sensitivity weights as defined
+        sensitivity_weights = {'High':      {'price': 0.7, 'distance': 0.3},
+                               'Medium':    {'price': 0.5, 'distance': 0.5},
+                               'Low':       {'price': 0.3, 'distance': 0.7}
+                               }
+        weight = sensitivity_weights[price_sensitivity]
+
+        # Store scores by store name and details, we introduce defaultdict to avoid key errors
+        store_scores = defaultdict(list)
+        detailed_scores = defaultdict(dict)
+
+        # Iterate over each item and their respective stores
+        for item in prices:
+            for store in item['stores']:
+                store_name = store['store'].lower()
+                store_price = store['price']
+
+                # Retrieve the distance for the current store, using float('inf') if not available
+                store_distance = distances.get(store_name, float('inf'))
+
+                # Calculate the score using both price and distance, taking into account the weight based on price sensitivity
+                score = (weight['price'] * store_price) + (weight['distance'] * store_distance)
+                store_scores[store_name].append(score)
+
+                # Append a dictionary with all store details plus the calculated score
+                if item['_id'] not in detailed_scores[store_name]:
+                    detailed_scores[store_name][item['_id']] = []
+                detailed_scores[store_name][item['_id']].append({
+                    'price': store_price,
+                    'distance': store_distance,
+                    'score': score
+                })
+
+        # Calculate average scores
+        average_scores = {store: sum(scores) / len(scores) for store, scores in store_scores.items()}
+
+        # Find the store with the lowest average score
+        recommended_store = min(average_scores, key=average_scores.get)
+        recommended_score = average_scores[recommended_store]
+
+        return recommended_store, recommended_score, detailed_scores, average_scores
+
+
+    # https://www.geeksforgeeks.org/program-distance-two-points-earth/
+
+    def calculate_distance(self, user_location, store_location):
+        from math import radians, cos, sin, sqrt, atan2
+
+        # Convert latitude and longitude from degrees to radians
+        lat1, lon1 = map(radians, user_location)
+        lat2, lon2 = map(radians, store_location)
+
+        # Difference in coordinates
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+
+        # Haversine formula
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        # Earth's radius in kilometers
+        R = 6371.0
+
+        # Calculate the distance
+        distance = R * c
+        return distance
--- app/main.py
+++ app/main.py
@@ -3,9 +3,6 @@ kivy.require('2.3.0')
 
 # Standard library imports
 from bson.objectid import ObjectId
-from collections import defaultdict
-from datetime import datetime, timedelta
-import re
 
 # Third party imports
 from kivy.app import App
@@ -27,40 +24,39 @@ Logger.setLevel(logging.INFO)
 database = DatabaseOperations(db_name='receiptsys')
 
 class LoginScreen(Screen):
-    def show_error_popup(self, message):
-        popup = Popup(title='Login Error',
-                      content=Label(text=message),
-                      size_hint=(None, None), size=(400, 200))
-        popup.open()
-
     def login(self, username, password):
+        # Check if username is empty
         if not username:
-            self.show_error_popup('Username cannot be empty.')
+            App.get_running_app().show_error_popup('Error', 'Username cannot be empty.')
             return
 
-        user = database.db['user_profiles'].find_one({"username": username})
+        user = database.find_one('user_profiles', {'username': username})
 
+        # Check if the user exists
         if not user:
-            self.show_error_popup('Username does not exist.')
+            App.get_running_app().show_error_popup('Error', 'Username does not exist.')
             return
 
+        # Check if the password is correct
         if user.get("password") != password:
-            self.show_error_popup('Incorrect password.')
+            App.get_running_app().show_error_popup('Error', 'Incorrect password.')
             return
         
         else:
+            # Set the current user and user ID
             App.get_running_app().current_user = username
             App.get_running_app().current_user_id = user.get('_id')
+            # Redirect to the main screen
             self.manager.current = 'main'          
 
 class MainScreen(Screen):
     pass
 
 class NewUser(Screen):
-    price_sensitivity = None  # Default value; adjust as needed
+    price_sensitivity = None
 
     def create_user_profile(self, username, password, price_sensitivity):
-        # This assumes that `self.price_sensitivity` holds the value from button selection
+        # This assumes that 'self.price_sensitivity' holds the value from button selection
         price_sensitivity = self.price_sensitivity if hasattr(self, 'price_sensitivity') and self.price_sensitivity is not None else 'Medium'  # Default to 'Medium' if not set
 
         # Create or update the user profile with the provided information
@@ -74,17 +70,14 @@ class NewUser(Screen):
         }
 
         # Check if the user already exists to decide on create or update
-        existing_user = database.db['user_profiles'].find_one({"username": username})
+        existing_user = database.find_one('user_profiles', {'username': username})
         if existing_user:
-            popup_content = Label(text="User already exists. Please log in or use a different username.")
-            popup = Popup(title="User already exists",
-                          content=popup_content,
-                          size_hint=(None, None), size=(400, 200))
-            popup.open()
+            App.get_running_app().show_error_popup('User already exists', 
+                                                   'Please log in or use a different username.')
             return
         else:
             # Insert new user profile
-            database.db['user_profiles'].insert_one(profile)
+            database.insert_one('user_profiles', profile)
             print("Created new user profile.")
 
         self.manager.current = 'login'  # Redirect to main screen after creating profile
@@ -106,127 +99,39 @@ class CreBasketScreen(Screen):
     def check_prices(self, selected_days, basket_items):
         if not self.validate_days(selected_days):
             return
-
+        
         days_back = None if selected_days == "All time" else int(selected_days.split()[0])
+
         user_id = App.get_running_app().current_user_id
-        user_profile = database.db['user_profiles'].find_one({'_id': user_id})
+        user_profile = database.find_one('user_profiles', {'_id': user_id})
         price_sensitivity = user_profile['preferences']['price_sensitivity']
 
-        prices = self.find_lowest_prices_for_basket(basket_items, days_back)
-        print(prices)
-        store_distances = self.fetch_store_distances(prices)
-        rec_store, rec_score, detail_scores, avg_scores = self.calculate_store_scores(prices, store_distances, price_sensitivity)
+        # Fetch the lowest prices for the basket items
+        prices = database.find_lowest_prices_for_basket(basket_items, days_back)
+        # Fetch store distances
+        store_distances = database.fetch_store_distances(self.user_location, prices)
+        # If no store with prices is found, show an error popup
+        if not store_distances:
+            App.get_running_app().show_error_popup('Error', 'No store found.')
+            return
+        # Calculate store scores
+        rec_store, rec_score, detail_scores, avg_scores = database.calculate_store_scores(prices, store_distances, price_sensitivity)
 
         self.display_price_results(rec_store, rec_score, detail_scores, avg_scores)
 
+    # Validate the selected number of days
     def validate_days(self, selected_days):
         valid_values = ['1 day', '7 days', '14 days', '30 days', 'All time']
         if selected_days not in valid_values:
-            self.show_popup('Invalid Selection', 'Please select a valid number of days.')
+            App.get_running_app().show_error_popup('Invalid Selection', 'Please select a valid number of days.')
             return False
         return True
 
-    def find_lowest_prices_for_basket(self, basket_items, days_back=None):
-        # groceries = db['groceries']
-        results = []
-        for item in basket_items:
-            if not item.strip():
-                continue
-            results.append(self.query_grocery_prices(item.upper(), days_back))
-        return results
-
-    def query_grocery_prices(self, item_name, days_back):
-        groceries = database.db['groceries']
-        start_date = datetime.now() - timedelta(days=days_back) if days_back else None
-        query = {"name": item_name, "price_history.date": {"$gte": start_date}} if start_date else {"name": item_name}
-        pipeline = [
-            {'$match': query},
-            {'$unwind': '$price_history'},
-            {'$sort': {'price_history.store': 1, 'price_history.date': -1}},
-            {'$group': {
-                '_id': '$price_history.store',
-                'latestDate': {'$first': '$price_history.date'},
-                'minPrice': {'$min': '$price_history.price'},
-                'itemName': {'$first': '$name'}
-            }},
-            {'$group': {
-                '_id': '$itemName',
-                'stores': {'$push': {'store': '$_id', 'date': '$latestDate', 'price': '$minPrice'}}
-            }}
-        ]
-        result = list(groceries.aggregate(pipeline))
-        return result[0] if result else {'_id': item_name, 'stores': []}
-
-    def fetch_store_distances(self, prices):
-        involved_stores = {store['store'].lower() for price_info in prices for store in price_info['stores']}
-
-        # Prepare an $or query with case-insensitive regex matches for each store
-        regex_query = [{'name': re.compile(f'^{re.escape(store)}$', re.IGNORECASE)} for store in involved_stores]
-        temp_data = database.db['stores'].find({'$or': regex_query})
-        store_data = {store['name'].lower(): store for store in temp_data}
-
-        # Calculate distances using names directly from store_data
-        distances = {}
-        for store_name, store_info in store_data.items():
-            if 'location' in store_info and 'coordinates' in store_info['location']:
-                store_location = store_info['location']['coordinates']
-                distances[store_name] = self.calculate_distance(self.user_location, store_location)
-            else:
-                print(f"Location data missing for store: {store_name}")
-                distances[store_name] = None  # or handle as needed
-
-        return distances
-
-    def calculate_store_scores(self, prices, distances, price_sensitivity):
-        # Log the distances to verify their availability
-        print("Distances available:", distances)
-
-        # Sensitivity weights as defined
-        sensitivity_weights = {'High':      {'price': 0.7, 'distance': 0.3},
-                               'Medium':    {'price': 0.5, 'distance': 0.5},
-                               'Low':       {'price': 0.3, 'distance': 0.7}
-                               }
-        weight = sensitivity_weights[price_sensitivity]
-
-        # Store scores by store name and details, we introduce defaultdict to avoid key errors
-        store_scores = defaultdict(list)
-        detailed_scores = defaultdict(dict)
-
-        # Iterate over each item and their respective stores
-        for item in prices:
-            for store in item['stores']:
-                store_name = store['store'].lower()
-                store_price = store['price']
-
-                # Retrieve the distance for the current store, using float('inf') if not available
-                store_distance = distances.get(store_name, float('inf'))
-
-                # Calculate the score using both price and distance, taking into account the weight based on price sensitivity
-                score = (weight['price'] * store_price) + (weight['distance'] * store_distance)
-                store_scores[store_name].append(score)
-
-                # Append a dictionary with all store details plus the calculated score
-                if item['_id'] not in detailed_scores[store_name]:
-                    detailed_scores[store_name][item['_id']] = []
-                detailed_scores[store_name][item['_id']].append({
-                    'price': store_price,
-                    'distance': store_distance,
-                    'score': score
-                })
-
-        # Calculate average scores
-        average_scores = {store: sum(scores) / len(scores) for store, scores in store_scores.items()}
-
-        # Find the store with the lowest average score
-        recommended_store = min(average_scores, key=average_scores.get)
-        recommended_score = average_scores[recommended_store]
-
-        return recommended_store, recommended_score, detailed_scores, average_scores
-
-
+    # Display the price check results
     def display_price_results(self, recommended_store, recommended_score, detailed_scores, average_scores):
         recommended_texts = [f"Recommended Store: {recommended_store.title()} (Best Average Score, {recommended_score:.2f})"]
         display_texts = []
+        # Display the recommended store and its details
         for store, item in detailed_scores.items():
             display_texts.append(f"Store: {store.title()} \n  Average Score: {average_scores[store]:.1f}")
             keys = list(item.keys())
@@ -240,75 +145,48 @@ class CreBasketScreen(Screen):
         App.get_running_app().price_check_results = str(recommended_texts[0])
         App.get_running_app().detailed_store_info = "\n".join(display_texts)
         self.manager.current = 'price_results'
-
-    def show_popup(self, title, message):
-        popup = Popup(title=title, content=Label(text=message), size_hint=(None, None), size=(400, 200))
-        popup.open()
-
-    # https://www.geeksforgeeks.org/program-distance-two-points-earth/
-
-    def calculate_distance(self, user_location, store_location):
-        from math import radians, cos, sin, sqrt, atan2
-
-        # Convert latitude and longitude from degrees to radians
-        lat1, lon1 = map(radians, user_location)
-        lat2, lon2 = map(radians, store_location)
-
-        # Difference in coordinates
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
-
-        # Haversine formula
-        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-        c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-        # Earth's radius in kilometers
-        R = 6371.0
-
-        # Calculate the distance
-        distance = R * c
-        return distance
     
 class PriceResultsScreen(Screen):
+    # Display the recommended store and detailed store information
     def show_all_stores(self):
         detailed_info = App.get_running_app().detailed_store_info
         self.ids.results_label.text = detailed_info
     
+    # Display only the recommended store
     def refresh_store(self):
         recommended_store = App.get_running_app().price_check_results
         self.ids.results_label.text = recommended_store
 
 class ViewProfileScreen(Screen):
-    def get_price_sensitivity(self):
-        return self.price_sensitivity
-    
+    # Fetch the user's price sensitivity from the database
+    # on_pre_enter is used to ensure the data is fetched before the screen is entered
     def on_pre_enter(self):
         user_id = App.get_running_app().current_user_id
-        user_profile = database.db['user_profiles'].find_one({'_id': user_id})
+        user_profile = database.find_one('user_profiles', {'_id': user_id})
         if user_profile:
             self.price_sensitivity = user_profile['preferences']['price_sensitivity']
         else:
             print("User profile not found.")
     
+    # Display the user's username and price sensitivity
+    # on_enter is used to ensure the data is displayed when the screen is entered
     def on_enter(self):
         self.ids.username_label.text = f"Username: {App.get_running_app().get_username()}"
         self.ids.price_sensitivity_label.text = f"Price Sensitivity: {self.price_sensitivity}"
 
 class ChangePriceSensScreen(Screen):
+    # Initialize the price sensitivity property
     def set_price_sensitivity(self, sensitivity):
         self.price_sensitivity = sensitivity
         print(f"Price sensitivity set to: {self.price_sensitivity}")
-
-    def get_price_sensitivity(self):
-        return self.price_sensitivity
     
+    # Save the updated price sensitivity to the database
     def save_price_sensitivity(self):
         user_id = App.get_running_app().current_user_id
-        price_sensitivity = self.get_price_sensitivity()
-        database.db['user_profiles'].update_one(
-            {'_id': user_id},
-            {'$set': {'preferences.price_sensitivity': price_sensitivity}}
-        )
+        database.update_one('user_profiles', 
+                            {'_id': user_id}, 
+                            {'preferences.price_sensitivity': self.price_sensitivity}
+                            )
         print("Price sensitivity updated.")
         self.manager.current = 'view_profile'
 class WindowManager(ScreenManager):
@@ -319,15 +197,6 @@ class MyBasketApp(App):
     current_user_id = None  # Global variable to keep track of the current user's ID
     price_check_results = StringProperty("")  # Property to hold the price check results
     detailed_store_info = ObjectProperty(None)  # Property to hold detailed store information
-
-    # def on_start(self):
-    #     gps.configure(on_location=self.on_gps_location)
-    #     gps.start()
-
-    # def on_gps_location(self, **kwargs):
-    #     kwargs['lat'], kwargs['lon']
-    #     print(f"Latitude: {kwargs['lat']}, Longitude: {kwargs['lon']}")
-
 
     def build(self):
         self.title = 'My Basket App'
@@ -347,28 +216,34 @@ class MyBasketApp(App):
         sm.current = initial_screen
         return sm
 
+    # Check if a user is logged in
     def check_user_session(self):
         return True if self.current_user else False
 
+    # Get the current user's ID
     def get_user_id(self):
         return self.current_user_id
-
-    # def get_user_receipts(user_id):
-    #     # Get all receipts for the user
-    #     receipts = database.db['receipts'].find({"user_id": user_id})
-    #     return list(receipts)
     
+    # Get the current user's username
     def get_username(self):
-        user_profile = database.db['user_profiles'].find_one({'_id': ObjectId(self.current_user_id)})
+        user_profile = database.find_one('user_profiles', {'_id': ObjectId(self.current_user_id)})
         if user_profile:
             return user_profile['username']
         return "Unknown"
     
+    # Get the current user's price sensitivity
     def get_price_sensitivity(self):
-        user_profile = database.db['user_profiles'].find_one({'_id': ObjectId(self.current_user_id)})
+        user_profile = database.find_one('user_profiles', {'_id': ObjectId(self.current_user_id)})
         if user_profile and 'preferences' in user_profile:
             return user_profile['preferences'].get('price_sensitivity', 'Not Set')
         return "Not Set"
+    
+    def show_error_popup(self, title, message):
+        popup = Popup(title=title, 
+                      content=Label(text=message), 
+                      size_hint=(None, None), 
+                      size=(400, 200))
+        popup.open()
 
     def logout(self):
         print(f"User {self.current_user} logged out.")
